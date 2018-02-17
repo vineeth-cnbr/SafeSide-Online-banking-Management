73ced384b98ee3872a31e293ed96476572a40451
Vineeth Chandran <vineeth.cnbr@gmail.com>
2018-02-18 01:36:30 +0530

    18/02/2016 01:34

--- functions/index.js
+++ functions/index.js
@@ -8,10 +8,6 @@ var bodyParser = require('body-parser');
 app.use(express.static( __dirname + '/public'));
 app.set('view engine', 'ejs');
 
-<<<<<<< HEAD
-app.get("/hi", (req, res) => {
-    res.send("hello")
-=======
 //admin.initializeApp(functions.config().firebase
 
 var serviceAccount = require("./secret.json");
@@ -26,21 +22,10 @@ app.get("/hi", (req, res) => {
 
     res.send("hello");
 
->>>>>>> 4f3c1f8856338e04216a12c4cc01f0779fe48c2a
 })
 
 
 app.post("/signupsubmit", function (req,res) {
-<<<<<<< HEAD
-	admin.auth().createUser({
-	name: req.body.name,
-	email: req.body.email,
-	emailVerified: false,
-	password: req.body.pass
-	/*displayName: "John Doe",
-	photoURL: "http://www.example.com/12345678/photo.png",
-	disabled: false*/
-=======
   admin.auth().createUser({
   name: req.body.name,
   email: req.body.email,
@@ -49,7 +34,6 @@ app.post("/signupsubmit", function (req,res) {
 /* 
   photoURL: "http://www.example.com/12345678/photo.png",
   disabled: false*/
->>>>>>> 4f3c1f8856338e04216a12c4cc01f0779fe48c2a
 })
   .then(function(userRecord) {
     // See the UserRecord reference doc for the contents of userRecord.
@@ -59,8 +43,6 @@ app.post("/signupsubmit", function (req,res) {
     console.log("Error creating new user:", error);
   });
 })
-<<<<<<< HEAD
-=======
 
 app.post("/loginsubmit", function (req,res) {
 	var uid = "some-uid";
@@ -74,7 +56,6 @@ app.post("/loginsubmit", function (req,res) {
   		// ...
 	});
 })
->>>>>>> 4f3c1f8856338e04216a12c4cc01f0779fe48c2a
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
