2c74c05376644cfe20ce61f17de535afb03ed80a
prado.98 <pradeepsudars.98@gmail.com>
2018-02-20 12:10:43 +0530

    20/02 12:10 errors

--- functions/index.js
+++ functions/index.js
@@ -4,9 +4,16 @@ const admin = require("firebase-admin");
 var functions = require("firebase-functions");
 var firebase = require('firebase');
 var bodyParser = require('body-parser');
+var cookieSession = require('cookie-session')
 
 app.use(express.static( __dirname + '/public'));
 app.set('view engine', 'ejs');
+app.use(cookieSession({
+  keys: ['ThisIsSecret'],
+  // Cookie Options 
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours 
+}))
+
 
 app.get("/hi", (req, res) => {
 
@@ -14,7 +21,12 @@ app.get("/hi", (req, res) => {
 
 });
 
-app.get('/dashboard', (req, res) => {
+app.post("/login", (req, res) => {
+  console.log("uid: "+req.body.uid);
+  res.redirect("/dashboard")
+})
+
+app.get('/dashboard', (req, res) => { 
 	res.render('dashboard.ejs');
 });
 
